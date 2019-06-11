c03be053d6ec09e3411ac3052583453f070b0518
Dmitriy Karbyshev <dmitriy.karbyshev@oncam.com>
2019-06-11 19:07:29 +0300

    [#889] rename k8s-gke-service-account-assigner to k8s-gke-saa

--- helms/k8s-gke-saa/templates/_helpers.tpl
+++ helms/k8s-gke-saa/templates/_helpers.tpl
@@ -2,7 +2,7 @@
 {{/*
 Expand the name of the chart.
 */}}
-{{- define "k8s-gke-service-account-assigner.name" -}}
+{{- define "k8s-gke-saa.name" -}}
 {{- default .Chart.Name .Values.nameOverride | trunc 63 | trimSuffix "-" -}}
 {{- end -}}
 
@@ -11,7 +11,7 @@ Create a default fully qualified app name.
 We truncate at 63 chars because some Kubernetes name fields are limited to this (by the DNS naming spec).
 If release name contains chart name it will be used as a full name.
 */}}
-{{- define "k8s-gke-service-account-assigner.fullname" -}}
+{{- define "k8s-gke-saa.fullname" -}}
 {{- if .Values.fullnameOverride -}}
 {{- .Values.fullnameOverride | trunc 63 | trimSuffix "-" -}}
 {{- else -}}
@@ -27,13 +27,13 @@ If release name contains chart name it will be used as a full name.
 {{/*
 Create chart name and version as used by the chart label.
 */}}
-{{- define "k8s-gke-service-account-assigner.chart" -}}
+{{- define "k8s-gke-saa.chart" -}}
 {{- printf "%s-%s" .Chart.Name .Chart.Version | replace "+" "_" | trunc 63 | trimSuffix "-" -}}
 {{- end -}}
 
 {{/*
 Create service account name used by the chart daemonset.
 */}}
-{{- define "k8s-gke-service-account-assigner.serviceAccountName" -}}
+{{- define "k8s-gke-saa.serviceAccountName" -}}
 {{- default .Chart.Name .Values.serviceAccountNameOverride | trunc 63 | trimSuffix "-" -}}
 {{- end -}}
--- terraform/modules/k8s/main.tf
+++ terraform/modules/k8s/main.tf
@@ -333,28 +333,28 @@ resource "helm_release" "monitoring" {
 ########################################################
 # k8s GKE Service Account Assigner
 ########################################################
-data "template_file" "gke_sa_assigner_values" {
-  template = "${file("${path.module}/templates/gke_sa_assigner.yaml")}"
+data "template_file" "gke_saa_values" {
+  template = "${file("${path.module}/templates/gke_saa.yaml")}"
   vars = {
-    default_scopes = "${var.gke_sa_assigner_default_scopes}"
-    default_sa     = "${var.gke_sa_assigner_default_sa}"
-    sa_name        = "${var.gke_sa_assigner_sa_name}"
-    image_repo     = "${var.gke_sa_assigner_image_repo}"
-    image_tag      = "${var.gke_sa_assigner_image_tag}"
-    host_port      = "${var.gke_sa_assigner_host_port}"
-    container_port = "${var.gke_sa_assigner_container_port}"
-    name           = "${var.gke_sa_assigner_name}"
+    default_scopes = "${var.gke_saa_default_scopes}"
+    default_sa     = "${var.gke_saa_default_sa}"
+    sa_name        = "${var.gke_saa_sa_name}"
+    image_repo     = "${var.gke_saa_image_repo}"
+    image_tag      = "${var.gke_saa_image_tag}"
+    host_port      = "${var.gke_saa_host_port}"
+    container_port = "${var.gke_saa_container_port}"
+    name           = "${var.gke_saa_name}"
   }
 }
 
-resource "helm_release" "gke_sa_assigner" {
-    name        = "gke-sa-assigner"
-    chart       = "k8s-gke-service-account-assigner"
+resource "helm_release" "gke_saa" {
+    name        = "gke-saa"
+    chart       = "k8s-gke-saa"
     version     = "${var.legion_infra_version}"
     repository  = "${data.helm_repository.legion.metadata.0.name}"
     namespace   = "kube-system"
 
     values = [
-      "${data.template_file.gke_sa_assigner_values.rendered}"
+      "${data.template_file.gke_saa.rendered}"
     ]
 }
