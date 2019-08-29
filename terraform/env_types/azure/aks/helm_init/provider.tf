84f46620d8969faeefb778f76ff47a98866f339c
Max Kochubey <maxkochubey@gmail.com>
2019-08-29 13:03:04 +0300

    [legion-platform/legion#980] Set of used providers and its options

---
+++ terraform/env_types/azure/aks/helm_init/provider.tf
@@ -0,0 +1,30 @@
+provider "azurerm" {
+  version         = "~> 1.32"
+  subscription_id = var.azure_subscription_id
+  tenant_id       = var.azure_tenant_id
+  client_id       = var.azure_client_id
+  client_secret   = var.azure_client_secret
+}
+
+provider "kubernetes" {
+  version                  = "~> 1.9"
+  config_context_auth_info = local.config_context_auth_info
+  config_context_cluster   = local.config_context_cluster
+}
+
+provider "helm" {
+  version         = "~> 0.10.2"
+  install_tiller  = true
+  namespace       = "kube-system"
+  service_account = "tiller"
+  tiller_image    = ""
+
+  kubernetes {
+    config_context_auth_info = local.config_context_auth_info
+    config_context_cluster   = local.config_context_cluster
+  }
+}
+
+provider "null" {
+  version = "~> 2.1"
+}
