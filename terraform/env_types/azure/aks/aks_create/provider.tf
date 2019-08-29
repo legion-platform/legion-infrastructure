39f1fc71a81670729bb3da1effeae0a274b3c83f
Max Kochubey <maxkochubey@gmail.com>
2019-08-29 12:46:54 +0300

    [#962] Set of used providers and its options

---
+++ terraform/env_types/azure/aks/aks_create/provider.tf
@@ -0,0 +1,26 @@
+provider "azurerm" {
+  version         = "~> 1.32"
+  subscription_id = var.azure_subscription_id
+  tenant_id       = var.azure_tenant_id
+  client_id       = var.azure_client_id
+  client_secret   = var.azure_client_secret
+}
+
+provider "aws" {
+  version                 = "~> 2.25"
+  region                  = var.aws_region
+  shared_credentials_file = var.aws_credentials_file
+  profile                 = var.aws_profile
+}
+
+provider "random" {
+  version = "~> 2.2"
+}
+
+provider "local" {
+  version = "~> 1.3"
+}
+
+provider "null" {
+  version = "~> 2.1"
+}
