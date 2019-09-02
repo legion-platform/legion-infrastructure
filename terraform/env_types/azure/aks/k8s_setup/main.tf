0d30457b33c928ef7938b2cad3269381848fd52a
Max Kochubey <maxkochubey@gmail.com>
2019-09-02 16:52:26 +0300

    [legion-platform/legion#980] Oauth module have been rebased from develop branch

--- terraform/env_types/azure/aks/k8s_setup/main.tf
+++ terraform/env_types/azure/aks/k8s_setup/main.tf
@@ -1,103 +1,93 @@
+data "azurerm_kubernetes_cluster" "aks" {
+  name                = var.cluster_name
+  resource_group_name = "${var.cluster_name}-rg"
+}
+
+data "azurerm_public_ip" "ext_ip" {
+  name                = "${var.cluster_name}-extip"
+  resource_group_name = data.azurerm_kubernetes_cluster.aks.node_resource_group
+  depends_on          = [data.azurerm_kubernetes_cluster.aks]
+}
+
+locals {
+  config_context_auth_info = data.azurerm_kubernetes_cluster.aks.kube_config.0.username
+  config_context_cluster   = var.cluster_name
+}
+
+module "get_tls" {
+  source = "../../../../modules/tls"
+  secrets_storage = var.secrets_storage
+  cluster_name    = var.cluster_name
+}
+
+
 ########################################################
 # K8S setup
 ########################################################
 module "base_setup" {
-  source                    = "../../../../modules/k8s/base_setup"
-  aws_profile               = "${var.aws_profile}"
-  aws_credentials_file      = "${var.aws_credentials_file}"
-  zone                      = "${var.zone}"
-  region                    = "${var.region}"
-  region_aws                = "${var.region_aws}"
-  project_id                = "${var.project_id}"
-  cluster_name              = "${var.cluster_name}"
-  secrets_storage           = "${var.secrets_storage}"
+  source          = "../../../../modules/k8s/base_setup"
+  cluster_name    = var.cluster_name
+  tls_key         = module.get_tls.tls_secret_key
+  tls_crt         = module.get_tls.tls_secret_crt
 }
 
 module "nginx-ingress" {
-  source                    = "../../../../modules/k8s/nginx-ingress"
-  zone                      = "${var.zone}"
-  region                    = "${var.region}"
-  project_id                = "${var.project_id}"
-  cluster_name              = "${var.cluster_name}"
-  allowed_ips               = ["${var.allowed_ips}"]
-  root_domain               = "${var.root_domain}"
-  dns_zone_name             = "${var.dns_zone_name}"
-  network_name              = "${var.network_name}"
-}
-module "dashboard" {
-  source                    = "../../../../modules/k8s/dashboard"
-  aws_profile               = "${var.aws_profile}"
-  aws_credentials_file      = "${var.aws_credentials_file}"
-  zone                      = "${var.zone}"
-  region                    = "${var.region}"
-  region_aws                = "${var.region_aws}"
-  project_id                = "${var.project_id}"
-  cluster_name              = "${var.cluster_name}"
-  root_domain               = "${var.root_domain}"
-  tls_secret_key            = "${module.base_setup.tls_secret_key}"
-  tls_secret_crt            = "${module.base_setup.tls_secret_crt}"
+  source        = "../../../../modules/k8s/nginx-ingress"
+  lb_ip_address = data.azurerm_public_ip.ext_ip.ip_address
+  replicas      = data.azurerm_kubernetes_cluster.aks.agent_pool_profile.0.count
 }
 
-module "dex" {
-  source                    = "../../../../modules/k8s/dex"
-  aws_profile               = "${var.aws_profile}"
-  aws_credentials_file      = "${var.aws_credentials_file}"
-  zone                      = "${var.zone}"
-  region                    = "${var.region}"
-  region_aws                = "${var.region_aws}"
-  project_id                = "${var.project_id}"
-  cluster_name              = "${var.cluster_name}"
-  legion_infra_version      = "${var.legion_infra_version}"
-  legion_helm_repo          = "${var.legion_helm_repo}"
-  root_domain               = "${var.root_domain}"
-  dns_zone_name             = "${var.dns_zone_name}"
-  github_org_name           = "${var.github_org_name}"
-  dex_github_clientid       = "${var.dex_github_clientid}"
-  dex_github_clientSecret   = "${var.dex_github_clientSecret}"
-  dex_client_secret         = "${var.dex_client_secret}"
-  dex_static_user_email     = "${var.dex_static_user_email}"
-  dex_static_user_pass      = "${var.dex_static_user_pass}"
-  dex_static_user_hash      = "${var.dex_static_user_hash}"
-  dex_static_user_name      = "${var.dex_static_user_name}"
-  dex_static_user_id        = "${var.dex_static_user_id}"
-  dex_client_id             = "${var.dex_client_id}"
-  dex_cookie_secret         = "${var.dex_cookie_secret}"
+# module "dashboard" {
+#   source                    = "../../../../modules/k8s/dashboard"
+#   cluster_name              = var.cluster_name
+#   root_domain               = var.root_domain
+#   tls_secret_key            = module.base_setup.tls_secret_key
+#   tls_secret_crt            = module.base_setup.tls_secret_crt
+# }
+
+module "auth" {
+  source                  = "../../../../modules/k8s/auth"
+  cluster_name            = var.cluster_name
+  root_domain             = var.root_domain
+  oauth_client_id         = var.oauth_client_id
+  oauth_client_secret     = var.oauth_client_secret
+  oauth_redirect_url      = "https://auth.${var.cluster_name}.${var.root_domain}/oauth2/callback"
+  oauth_oidc_issuer_url   = "${var.keycloak_url}/auth/realms/${var.keycloak_realm}"
+  oauth_oidc_audience     = var.keycloak_realm_audience
+  oauth_cookie_expire     = "168h0m0s"
+  oauth_cookie_secret     = var.oauth_cookie_secret
+  oauth_oidc_scope        = var.oauth_scope
 }
 
 module "monitoring" {
   source                    = "../../../../modules/k8s/monitoring"
-  aws_profile               = "${var.aws_profile}"
-  aws_credentials_file      = "${var.aws_credentials_file}"
-  zone                      = "${var.zone}"
-  region                    = "${var.region}"
-  region_aws                = "${var.region_aws}"
-  project_id                = "${var.project_id}"
-  cluster_name              = "${var.cluster_name}"
-  legion_helm_repo          = "${var.legion_helm_repo}"
-  legion_infra_version      = "${var.legion_infra_version}"
-  alert_slack_url           = "${var.alert_slack_url}"
-  root_domain               = "${var.root_domain}"
-  grafana_admin             = "${var.grafana_admin}"
-  grafana_pass              = "${var.grafana_pass}"
-  docker_repo               = "${var.docker_repo}"
-  monitoring_namespace      = "${var.monitoring_namespace}"
-  tls_secret_key            = "${module.base_setup.tls_secret_key}"
-  tls_secret_crt            = "${module.base_setup.tls_secret_crt}"
+  cluster_name              = var.cluster_name
+  legion_infra_version      = var.legion_infra_version
+  alert_slack_url           = var.alert_slack_url
+  root_domain               = var.root_domain
+  grafana_admin             = var.grafana_admin
+  grafana_pass              = var.grafana_pass
+  grafana_storage_class     = var.grafana_storage_class
+  docker_repo               = var.docker_repo
+  monitoring_namespace      = var.monitoring_namespace
+  tls_secret_key            = module.get_tls.tls_secret_key
+  tls_secret_crt            = module.get_tls.tls_secret_crt
 }
 
 module "istio" {
   source                    = "../../../../modules/k8s/istio"
-  root_domain               = "${var.root_domain}"
-  cluster_name              = "${var.cluster_name}"
-  monitoring_namespace      = "${var.monitoring_namespace}"
-  tls_secret_key            = "${module.base_setup.tls_secret_key}"
-  tls_secret_crt            = "${module.base_setup.tls_secret_crt}"
-  legion_helm_repo          = "${var.legion_helm_repo}"
-  legion_infra_version      = "${var.legion_infra_version}"
+  root_domain               = var.root_domain
+  cluster_name              = var.cluster_name
+  istio_namespace           = var.istio_namespace
+  monitoring_namespace      = var.monitoring_namespace
+  knative_namespace         = var.knative_namespace
+  tls_secret_key            = module.get_tls.tls_secret_key
+  tls_secret_crt            = module.get_tls.tls_secret_crt
+  legion_infra_version      = var.legion_infra_version
 }
 
-module "gke-saa" {
-  source                    = "../../../../modules/k8s/gke-saa"
-  legion_helm_repo          = "${var.legion_helm_repo}"
-  legion_infra_version      = "${var.legion_infra_version}"
-}
+# module "gke-saa" {
+#   source                    = "../../../../modules/k8s/gke-saa"
+#   legion_helm_repo          = var.legion_helm_repo
+#   legion_infra_version      = var.legion_infra_version
+# }
