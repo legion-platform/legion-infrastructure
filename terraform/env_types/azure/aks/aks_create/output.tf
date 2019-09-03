00eba037b22a7a999d91c867dbf340c37a00567b
Max Kochubey <maxkochubey@gmail.com>
2019-09-03 11:44:34 +0300

    [legion-platform/legion#962] Some output added for convenience

--- terraform/env_types/azure/aks/aks_create/output.tf
+++ terraform/env_types/azure/aks/aks_create/output.tf
@@ -1,3 +1,7 @@
+output "bastion_connection_string" {
+  value = module.aks_bastion_host.bastion_ip
+}
+
 output "k8s_api_address" {
-  value       = module.aks_cluster.k8s_api_address
+  value = module.aks_cluster.k8s_api_address
 }
