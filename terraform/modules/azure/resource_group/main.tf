4be7c182e362705e55890859fcf191be4c5cdeb7
Max Kochubey <maxkochubey@gmail.com>
2019-09-06 16:37:49 +0300

    [legion-platform/legion#962] We will use predefined Azure resource group

--- terraform/modules/azure/resource_group/main.tf
+++
@@ -1,9 +0,0 @@
-########################################################
-# Create Azure resource group for base AKS components
-########################################################
-
-resource "azurerm_resource_group" "k8s" {
-  name     = var.name
-  location = var.location
-  tags     = var.tags
-}
--- terraform/modules/azure/resource_group/output.tf
+++
@@ -1,9 +0,0 @@
-output "name" {
-  value       = azurerm_resource_group.k8s.name
-  description = "Name of created Azure resource group"
-}
-
-output "location" {
-  value       = azurerm_resource_group.k8s.location
-  description = "Location in which Azure resource group have been created"
-}
