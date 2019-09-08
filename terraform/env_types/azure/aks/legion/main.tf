6eab71abd80c1690409588cc453333ba295b01b9
Max Kochubey <maxkochubey@gmail.com>
2019-09-08 22:53:11 +0300

    [legion-platform/legion#981] Some necessary data have been added

--- terraform/env_types/azure/aks/legion/main.tf
+++ terraform/env_types/azure/aks/legion/main.tf
@@ -1,3 +1,13 @@
+data "azurerm_kubernetes_cluster" "aks" {
+  name                = var.cluster_name
+  resource_group_name = "${var.cluster_name}-rg"
+}
+
+locals {
+  config_context_auth_info = data.azurerm_kubernetes_cluster.aks.kube_config.0.username
+  config_context_cluster   = var.cluster_name
+}
+
 ########################################################
 # Legion setup
 ########################################################
