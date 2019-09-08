a0f94386d96c4a654b3461ce567fcb073bdff4ed
Max Kochubey <maxkochubey@gmail.com>
2019-09-08 22:50:32 +0300

    [legion-platform/legion#980] Extra options for Helm provider removed as unused

--- terraform/env_types/azure/aks/k8s_setup/provider.tf
+++ terraform/env_types/azure/aks/k8s_setup/provider.tf
@@ -8,15 +8,8 @@ provider "azurerm" {
 
 provider "helm" {
   version         = "=0.10.2"
-  #install_tiller  = true
   namespace       = "kube-system"
   service_account = "tiller"
-  #tiller_image    = var.tiller_image
-  #init_helm_home  = true
-
-  # kubernetes {
-  #   config_context = local.config_context_auth_info
-  # }
 }
 
 provider "aws" {
