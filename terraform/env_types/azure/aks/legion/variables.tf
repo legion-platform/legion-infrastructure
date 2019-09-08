9bcb2af470c7762e4eb65272547266c210062cac
Max Kochubey <maxkochubey@gmail.com>
2019-09-08 22:55:30 +0300

    [legion-platform/legion#981] Some variables are unused in Azure, so removed from azure-specific env

--- terraform/env_types/azure/aks/legion/variables.tf
+++ terraform/env_types/azure/aks/legion/variables.tf
@@ -1,44 +1,19 @@
 ##################
 # Common
 ##################
-variable "project_id" {
-  description = "Target project id"
-}
 variable "cluster_name" {
   default     = "legion"
   description = "Legion cluster name"
 }
-variable "config_context_auth_info" {
-  description = "Legion cluster context auth"
-}
-variable "config_context_cluster" {
-  description = "Legion cluster context name"
-}
-variable "aws_profile" {
-  description = "AWS profile name"
-}
-variable "aws_credentials_file" {
-  default     = "~/.aws/config"
-  description = "AWS credentials file location"
-}
-variable "zone" {
-  default     = "us-east1-b"
-  description = "Default zone"
-}
-variable "region" {
-  default = "us-east1"
-  description = "Region of resources"
-}
-variable "region_aws" {
-  default = "us-east-2"
-  description = "Region of AWS resources"
-}
+
 variable "secrets_storage" {
   description = "Cluster secrets storage"
 }
+
 variable "legion_helm_repo" {
   description = "Legion helm repo"
 }
+
 variable "root_domain" {
   description = "Legion cluster root domain"
 }
@@ -49,6 +24,7 @@ variable "root_domain" {
 variable "legion_version" {
   description = "Legion release version"
 }
+
 variable "docker_repo" {
   description = "Legion Docker repo url"
 }
