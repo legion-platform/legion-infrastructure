805f6886ef55d01f3a2c1a0e9c8d7905fd44e426
Max Kochubey <maxkochubey@gmail.com>
2019-09-12 14:01:55 +0300

    [legion-platform/legion#962] Extra tautology removed

--- terraform/modules/azure/monitoring/main.tf
+++ terraform/modules/azure/monitoring/main.tf
@@ -1,9 +1,9 @@
+# The WorkSpace name has to be unique across the whole of azure, not just the current subscription/tenant.
 resource "random_id" "workspace" {
   byte_length = 8
 }
 
 resource "azurerm_log_analytics_workspace" "k8s" {
-  # The WorkSpace name has to be unique across the whole of azure, not just the current subscription/tenant.
   name                = "${var.cluster_name}-${random_id.workspace.dec}"
   location            = var.location
   resource_group_name = var.resource_group
