cffea5ae2d4343dc32fbde161a82abec22d370f1
Max Kochubey <maxkochubey@gmail.com>
2019-09-17 19:08:02 +0300

    [legion-platform/legion#962] Changes from develop branch

--- terraform/modules/legion/variables.tf
+++ terraform/modules/legion/variables.tf
@@ -65,6 +65,21 @@ variable "legion_namespace" {
   description = "Legion k8s namespace"
 }
 
+variable "legion_training_namespace" {
+  default     = "legion-training"
+  description = "Legion training k8s namespace"
+}
+
+variable "legion_packaging_namespace" {
+  default     = "legion-packaging"
+  description = "Legion packaging k8s namespace"
+}
+
+variable "legion_deployment_namespace" {
+  default     = "legion-deployment"
+  description = "Legion deployment k8s namespace"
+}
+
 variable "docker_repo" {
   description = "Legion Docker repo url"
 }
@@ -109,12 +124,22 @@ variable "model_docker_url" {
   description = "Model docker url"
 }
 
-variable "model_examples_git_url" {
+variable "git_examples_uri" {
   description = "Model examples git url"
 }
 
-variable "model_reference" {
+variable "git_examples_reference" {
   description = "Model reference"
+}
+
+variable "git_examples_web_ui_link" {
+  description = "Git examples web UI Link for Legion connection"
+  default     = ""
+}
+
+variable "git_examples_description" {
+  description = "Git examples description for Legion connection"
+  default     = ""
 }
 
 variable "model_resources_cpu" {
@@ -125,8 +150,8 @@ variable "model_resources_mem" {
   description = "Model pod mem limit"
 }
 
-variable "jenkins_git_key" {
-  description = "Jenkins git key for model repo"
+variable "git_examples_key" {
+  description = "Git ssh key for git connection"
 }
 
 variable "legion_data_bucket" {
@@ -137,3 +162,6 @@ variable "collector_region" {
   description = "Collector's storage bucket region"
 }
 
+variable "mlflow_toolchain_version" {
+  description = "Version of legion-mlflow helm chart"
+}
