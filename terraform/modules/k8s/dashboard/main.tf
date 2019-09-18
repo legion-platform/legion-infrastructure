b023467c4717e97efd62081255306ae39bac25de
Max Kochubey <maxkochubey@gmail.com>
2019-09-18 13:42:29 +0300

    [legion-platform/legion#980] dashboard changes from develop branch

--- terraform/modules/k8s/dashboard/main.tf
+++ terraform/modules/k8s/dashboard/main.tf
@@ -1,22 +1,3 @@
-provider "helm" {
-  version         = "v0.10.0"
-  install_tiller  = false
-}
-
-provider "google" {
-  version = "~> 2.2"
-  region  = var.region
-  zone    = var.zone
-  project = var.project_id
-}
-
-provider "aws" {
-  version                 = "2.13"
-  region                  = var.region_aws
-  shared_credentials_file = var.aws_credentials_file
-  profile                 = var.aws_profile
-}
-
 ########################################################
 # Kubernetes Dashboard
 ########################################################
